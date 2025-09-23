a1b236f826e05ee1002a6beeae80e3522e8a50f9
Daniel Do <dodaniel006@gmail.com>
2025-09-23 12:54:37 -0700

    Add comments to IDevice

--- src/code/IDevice.java
+++ src/code/IDevice.java
@@ -1,4 +1,7 @@
 /**
+ * Abstract class representing a generic IDevice with a specific purpose.
+ * Has a getter for the purpose and an abstract method to print details.
+ *
  * @author David Martinez, Daniel Do
  * @version 1.0
  */
