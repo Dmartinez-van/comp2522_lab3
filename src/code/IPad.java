5d2429ae22bce1c7b291c39f424f1d6e39db3c0a
Daniel Do <dodaniel006@gmail.com>
2025-09-25 11:27:01 -0700

    IPad changes

--- src/code/IPad.java
+++ src/code/IPad.java
@@ -2,9 +2,59 @@
  * @author David Martinez, Daniel Do
  * @version 1.0
  */
-public class IPad extends IDevice
+public final class IPad extends IDevice
 {
-    public IPad()
+
+    private boolean hasCase;
+    private String OSVersion;
+
+    public IPad(final boolean hasCase,
+                final String OSVersion)
     {
+        super("learning");
+        this.hasCase = hasCase;
+        this.OSVersion = OSVersion;
+    }
+
+    void printDetails()
+    {
+        final StringBuilder details;
+
+        details = new StringBuilder();
+
+        System.out.println(details.toString());
+    }
+
+    /**
+     * Returns a String representation of this IPad object, including whether it has a case and its OS version.
+     * The format is: IPad{hasCase=true, OSVersion='16.4.1'} plus the superclass details.
+     *
+     * @return a String describing this IPad instance
+     */
+    @Override
+    public String toString()
+    {
+        final StringBuilder ipadDetails;
+        ipadDetails = new StringBuilder();
+
+        ipadDetails.append(super.toString());
+        ipadDetails.append("IPad{");
+        ipadDetails.append("hasCase=").append(hasCase);
+        ipadDetails.append(", OSVersion='").append(OSVersion).append('\'');
+        ipadDetails.append("} ");
+
+        return ipadDetails.toString();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof IPad)) return false;
+        if (!super.equals(o)) return false;
+
+        IPad iPad = (IPad) o;
+
+        if (hasCase != iPad.hasCase) return false;
+        return OSVersion != null ? OSVersion.equals(iPad.OSVersion) : iPad.OSVersion == null;
     }
 }
